c2c6ef4959c3e4f448e468ed56fbd4ee101f26a0
Alejandro Viñas Portilla <114479197+AlejandroVinas@users.noreply.github.com>
2024-04-07 21:53:08 +0200

    Add files via upload

---
+++ INDEX/api.js
@@ -0,0 +1,27 @@
+
+import fetch from 'node-fetch';//este es for ESM module newer
+//const fetch = require('node-fetch')//CommonJS older
+//const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));// solución for both compatibility
+let API = 'http://localhost:8000';
+
+export async function getdata() {
+const res = await fetch(API);
+const data = await res.json();
+try {
+ //console.log(data);
+ return data;
+} catch (e) {
+ console.log('Some error');
+ console.log(e);
+ process.exit(0);
+}
+}
+ 
+//console.log(api_data);
+/*getdata().then(result=> {
+  console.log(data);
+}).catch(error => {
+  console.log('no va');
+});*/
+//console.log(api_data);*/
+
